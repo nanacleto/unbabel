b2d17767947f4b63b8ce41e46436f611824b5e56
nanacleto <35687704+nanacleto@users.noreply.github.com>
2018-10-02 22:22:07 +0100

    update3

--- request.py
+++ request.py
@@ -18,26 +18,12 @@ class unbable:
 
    x ={"country_list": ["BE", "DE", "NY"]}
 
-<<<<<<< HEAD
-
-=======
->>>>>>> fcbc69580d679f4100e6998a1b008c62799759ce
-   print  headers
-   print  x
-
-   response = rq.post(url, headers=headers, json=x)
-
-<<<<<<< HEAD
-
 
    print  headers
    print  x
 
    response = rq.post(url, headers=headers, json=x)
 
-
-=======
->>>>>>> fcbc69580d679f4100e6998a1b008c62799759ce
    print response.status_code
    print response.content
    print response.elapsed
